f93ddf8b561485439b350e4d7aaf6c9c00f1428d
Miguel Isrrael Teran <misrrael@purdue.edu>
2025-10-28 01:38:09 -0400

    Fill out YAML file, project top-level source file, and config.json

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_intro_ii_audio_synth (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,19 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out[7:1]  = '0;  // Unused dedicated outputs
+  assign uio_out = '0;  // Not using bidirectional output path
+  assign uio_oe  = '0;  // Will use ALL bidirectional pins as inputs
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in[7:6], 1'b0};
+
+  // Instantiate project
+  audio_synth_top audio_synth_proj (
+    .clk(clk),
+    .n_rst(rst_n),
+    .keypad_i({uio_in[5:0], ui_in}),
+    .pwm(uo_out[0])
+  );
 
 endmodule
